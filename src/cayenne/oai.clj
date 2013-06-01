afc0abde0b79fd65d009dc921d02a25b40a7b316
Karl Jonathan Ward <karl.j.ward@gmail.com>
2013-06-01 16:10:54 +0100

    For now grab and process all resumptive OAI-PMH requests that follow a particular initial request on the same thread as the initial request.

--- src/cayenne/oai.clj
+++ src/cayenne/oai.clj
@@ -2,8 +2,9 @@
   (:require [cayenne.xml :as xml]
             [cayenne.conf :as conf]
             [clj-http.client :as client]
-            [clj-time.core :as time]
+            [clj-time.core :as ctime]
             [clj-time.periodic :as ptime]
+            [clj-time.format :as ftime]
             [clojure.string :as string])
   (:use [clojure.java.io :only [file reader writer]])
   (:use [cayenne.job])
@@ -52,12 +53,11 @@
         xml-file (file dir-path file-name)
         params (if token
                  {"resumptionToken" token}
-                 (-> 
-                  {"metadataPrefix" (:type service)
-                   "verb" "ListRecords"}
-                  (?> #(:set-spec service) assoc "setspec" (:set-spec service))
-                  (?> from assoc "from" from)
-                  (?> until assoc "until" until)))]
+                 (-> {"metadataPrefix" (:type service)
+                      "verb" "ListRecords"}
+                     (?> #(:set-spec service) assoc "setspec" (:set-spec service))
+                     (?> from assoc "from" from)
+                     (?> until assoc "until" until)))]
     (let [conn-mgr (conf/get-service :conn-mgr)
           resp (client/get (:url service) {:query-params params
                                            :throw-exceptions false
@@ -69,9 +69,9 @@
           (.mkdirs dir-path)
           (spit xml-file (:body resp))
           (when parser-fn 
-            (process-oai-xml-file-async parser-fn task-fn xml-file result-set "record"))
+            (process-oai-xml-file parser-fn task-fn xml-file result-set "record"))
           (when-let [token (resumption-token (:body resp))]
-            (grab-oai-xml-file-async service from until (inc count) token parser-fn task-fn result-set)))))))
+            (recur service from until (inc count) token parser-fn task-fn result-set)))))))
 
 (defn grab-oai-xml-file-async [service from until count token parser-fn task-fn result-set]
   (when debug-grabbing
@@ -99,14 +99,24 @@
                                  parser nil}}]
   (grab-oai-xml-file-async service from until 1 nil parser task name))
 
+(defn str-date->parts [d]
+  (map #(Integer/parseInt %) (string/split d #"-")))
+
+(def oai-date-format (ftime/formatter "yyyy-MM-dd"))
+
 (defn run-range [service & {:keys [from until task parser name separation]
                             :or {task nil
                                  parser nil
-                                 separation (time/days 1)}}]
-  (let [from-date (apply time/date-time (string/split from "-"))
-        until-date (apply time/date-time (string/split until "-"))]
-    (doseq [from-point (take-while #(time/before? % until-date) 
+                                 separation (ctime/days 1)}}]
+  (let [from-date (apply ctime/date-time (str-date->parts from))
+        until-date (apply ctime/date-time (str-date->parts until))]
+    (doseq [from-point (take-while #(ctime/before? % until-date) 
                                    (ptime/periodic-seq from-date separation))]
-      (run service from-point (time/plus from-point separation) task parser name separation))))
+      (run service 
+           :from (ftime/unparse oai-date-format from-point)
+           :until (ftime/unparse oai-date-format (ctime/plus from-point separation))
+           :task task 
+           :parser parser 
+           :name name))))
     
 
