22464bb74db906729e80a304f54aaae0c11afab7
Karl Jonathan Ward <karl.j.ward@gmail.com>
2013-07-03 17:24:06 +0100

    Code to retrieve geoname object's literal name.

---
+++ src/cayenne/tasks/geoname.clj
@@ -0,0 +1,21 @@
+(ns cayenne.tasks.geoname
+  (:require [cayenne.rdf :as rdf]
+            [clojure.core.memoize :as memoize]))
+
+(defn get-geoname-name [url]
+  (let [rdf-model (rdf/document->model (java.net.URL. url))
+        name-property (rdf/get-property "http://www.geonames.org/ontology#"
+                                        rdf-model
+                                        "name")]
+    (-> rdf-model
+        (rdf/select :predicate name-property)
+        (rdf/objects)
+        (first)
+        (str))))
+
+(def get-geoname-name-memo (memoize/memo-lru get-geoname-name))
+
+(defn clear! []
+  (memoize/memo-clear! get-geoname-name-memo))
+
+  
