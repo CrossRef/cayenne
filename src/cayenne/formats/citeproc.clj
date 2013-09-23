77555338624f82d81b24ba2fefe7133f736d5019
Karl Jonathan Ward <karl.j.ward@gmail.com>
2013-09-23 10:37:52 +0100

    Include additional info in citeproc format.

--- src/cayenne/formats/citeproc.clj
+++ src/cayenne/formats/citeproc.clj
@@ -35,10 +35,21 @@
   (let [contribs (string/split (get solr-doc k) #", ")]
     (map ->citeproc-contrib contribs)))
 
+(defn ->citeproc-licenses [solr-doc]
+  (map #(if %2 {:URL %1 :start %2} {:URL %1})
+       (get solr-doc "license_url")
+       (get solr-doc "license_start")))
+
+(defn ->citeproc-links [solr-doc]
+  (map #(if %2 {:URL %1 :content-type %2} {:URL %1})
+       (get solr-doc "full_text_url")
+       (get solr-doc "full_text_type")))
+
 (defn ->citeproc [solr-doc]
   {:source (get solr-doc "source")
    :volume (get solr-doc "hl_volume")
    :issue (get solr-doc "hl_issue")
+   :prefix (get solr-doc "owner_prefix")
    :DOI (doi-id/extract-long-doi (get solr-doc "doi"))
    :URL (get solr-doc "doi")
    :ISBN (map isbn-id/extract-isbn (get solr-doc "isbn"))
@@ -55,9 +66,13 @@
    :chair (->citeproc-contribs solr-doc "hl_chairs")
    :contributor (->citeproc-contribs solr-doc "hl_contributors")
    :translator (->citeproc-contribs solr-doc "hl_translators")
+   :publisher (get solr-doc "publisher")
    :page (str (get solr-doc "hl_first_page")
               "-" 
               (get solr-doc "hl_last_page"))
    :type (get solr-doc "type")
    :subject (get solr-doc "category")
+   :archive (get solr-doc "archive")
+   :license (->citeproc-licenses solr-doc)
+   :link (->citeproc-links solr-doc)
    :score (get solr-doc "score")})
