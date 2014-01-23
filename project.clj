7f9c5fa4ae1d4494523b98291f5c796ca2f1bf41
Karl Jonathan Ward <karl.j.ward@gmail.com>
2014-01-23 12:13:43 +0000

    Give project a 'crossref' group id.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cayenne "0.1.0-SNAPSHOT"
+(defproject crossref/cayenne "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://github.com/CrossRef/cayenne"
   :license {:name "Eclipse Public License"
