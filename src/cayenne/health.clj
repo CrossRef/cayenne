50ad5adcf741e1319a0f8681e22390e3d128488a
Karl Jonathan Ward <karl.j.ward@gmail.com>
2014-01-25 14:11:09 +0000

    More metadata health checks.
    Coverage of contributor names, funders and journals.

--- src/cayenne/health.clj
+++ src/cayenne/health.clj
@@ -15,6 +15,14 @@
     (when (empty? (i/get-item-ids journal :issn))
       journal)))
 
+(defn journal-has-volume [item]
+  (when-let [journal (i/find-item-of-subtype :journal)]
+    (not (empty? (i/find-item-of-subtype :journal-volume)))))
+
+(defn journal-has-issue [item]
+  (when-let [journal (i/find-item-of-subtype :journal)]
+    (not (empty? (i/find-item-of-subtype :journal-issue)))))
+
 (defn full-publication-date [item]
   (let [pub-dates (concat
                    (i/get-tree-rel item :published-online)
@@ -22,6 +30,9 @@
     (filter
      #(or (nil? (:day %)) (nil? (:month %)) (nil? (:year %)))
      pub-dates)))
+
+(defn funding-information [item]
+  (not (empty? (i/get-tree-rel item :funder))))
 
 (defn funders-have-ids [item]
   (let [funders (i/get-tree-rel item :funder)]
@@ -35,28 +46,74 @@
      #(empty? (i/get-item-rel item :awarded))
      funders)))
 
-(defn full-contributor-names [item]
-  ())
+(defn contributors-have-full-names [item]
+  (let [contributors (i/find-items-of-type item :person)]
+    (filter
+     #(or (nil? (:first-name %)) (nil? (:last-name %)))
+     contributors)))
+
+(defn contributors-have-orcids [item]
+  (let [contributors (i/find-items-of-type item :person)]
+    (filter
+     #(empty? (i/get-item-ids % :orcid))
+     contributors)))
+
+(defn contributors-have-affiliations [item]
+  (let [contributors (i/find-items-of-type item :person)]
+    (filter #(nil? (:affiliation %)) contributors)))
+
+(defn contributors-no-bad-punctuation [item]
+  (let [contributors (i/find-items-of-type item :person)
+        bad-punctuation #"[,;\:\"'><@&]"]
+    (filter #(or (re-find bad-punctuation (:first-name %))
+                 (re-find bad-punctuation (:last-name %)))
+            contributors)))
+
+(defn contributor-family-names-no-whitespace [item]
+  (let [contributors (i/find-items-of-type item :person)
+        family-names (map :last-name contributors)]
+    (filter #(re-find #"\s" %) family-names)))
 
 (def checks
-  [{:id :general.standard-case-titles
+  [{:id :misc.funding-information
+    :description "Some funding information should be listed"
+    :fn funding-information}
+   {:id :misc.standard-case-titles
     :description "Titles should not be all upper-case"
     :fn title-case}
    {:id :journal.has-issn
     :description "Journals should have at least one ISSN"
     :fn journal-has-issn}
-   {:id :general.full-publication-dates
+   {:id :journal.has-volume
+    :description "Journals should have a volume number"
+    :fn journal-has-volume}
+   {:id :journal.has-issue
+    :description "Journals should have an issue number"
+    :fn journal-has-issue}
+   {:id :misc.full-publication-dates
     :description "Publication dates should have a day, month and year"
     :fn full-publication-date}
-   {:id :funder.has-id
-    :description "Funders should have FundRef IDs"
+   {:id :funder.has-doi
+    :description "Funders should have FundRef Funder DOIs"
     :fn funders-have-ids}
    {:id :funder.has-award
     :description "Funders should have at least one award"
     :fn funders-have-awards}
-   {:id :contributor.full-name
+   {:id :contributor.has-full-name
     :description "Contributors should have a given and family name"
-    :fn full-contributor-names}])
+    :fn contributors-have-full-names}
+   {:id :contributor.has-orcid
+    :description "Contributors should have an ORCID"
+    :fn contributors-have-orcids}
+   {:id :contributor.has-affiliation
+    :description "Contributors should have an affiliation"
+    :fn contributors-have-affiliations}
+   {:id :contributor.no-punctuation
+    :description "Contributors should have names without needless punctuation"
+    :fn contributors-no-bad-punctuation}
+   {:id :contributor.single-family-name
+    :description "Contributor family names should not contain whitespace"
+    :fn contributor-family-names-no-whitespace}])
 
 (defn check-tree 
   "Returns a report of checks performed against an item tree."
