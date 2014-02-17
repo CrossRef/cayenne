0e926139173bde3f465910a8a38177bd8a527e0b
Karl Jonathan Ward <karl.j.ward@gmail.com>
2014-02-17 11:14:41 +0000

    Allow style, language and format to vary in ->citation.

--- src/cayenne/formats/citation.clj
+++ src/cayenne/formats/citation.clj
@@ -2,7 +2,7 @@
   (:import [de.undercouch.citeproc.csl 
             CSLItemDataBuilder CSLDateBuilder CSLNameBuilder 
             CSLName CSLItemData CSLType]
-           [de.undercouch.citeproc CSL]))
+           [de.undercouch.citeproc CSL ListItemDataProvider]))
 
 (defn make-csl-issued-date [metadata]
   (let [year (get-in metadata [:issued :date-parts 0 0])
@@ -51,8 +51,10 @@
                               :or {style "apa"
                                    language "en-US"
                                    format "text"}}]
-  (-> (CSL/makeAdhocBibliography 
-       style 
-       format
-       (into-array CSLItemData [(->csl-item metadata)]))
-      (.makeString)))
+  (let [item-data (->csl-item metadata)
+        item-provider (ListItemDataProvider.
+                       (into-array CSLItemData [item-data]))
+        csl (doto (CSL. item-provider style language)
+              (.setOutputFormat format)
+              (.registerCitationItems (into-array String [(.getId item-data)])))]
+    (-> csl (.makeBibliography) (.makeString))))
