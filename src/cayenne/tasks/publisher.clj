0d8c2895f121a2b3d42cb2c39e5ed65daf4df40f
Karl Jonathan Ward <karl.j.ward@gmail.com>
2014-02-17 10:56:34 +0000

    publishers.clj: Move with-mongo into more general scope

--- src/cayenne/tasks/publisher.clj
+++ src/cayenne/tasks/publisher.clj
@@ -17,42 +17,41 @@
             [somnium.congomongo :as m]))
 
 (defn ensure-publisher-indexes! [collection-name]
-  (m/with-mongo (conf/get-service :mongo)
-    (m/add-index! collection-name [:id])
-    (m/add-index! collection-name [:tokens])
-    (m/add-index! collection-name [:prefixes])
-    (m/add-index! collection-name [:names])))
+  (m/add-index! collection-name [:id])
+  (m/add-index! collection-name [:tokens])
+  (m/add-index! collection-name [:prefixes])
+  (m/add-index! collection-name [:names]))
 
 (defn insert-publisher!
   "Upsert a publisher, combining multiple prefixes."
   [collection id prefix name location]
-  (m/with-mongo (conf/get-service :mongo)
-    (m/update! collection
-               {:id id}
-               {"$set" {:id id
-                        :location (string/trim location)}
-                "$addToSet" {:prefixes prefix
-                             :tokens {"$each" (util/tokenize-name name)}
-                             :names (string/trim name)}})))
+  (m/update! collection
+             {:id id}
+             {"$set" {:id id
+                      :location (string/trim location)}
+              "$addToSet" {:prefixes prefix
+                           :tokens {"$each" (util/tokenize-name name)}
+                           :names (string/trim name)}}))
 
 (defn load-publishers [collection]
-  (ensure-publisher-indexes! collection)
-  (doseq [prefix-number (range 1000 100000)]
-    (let [prefix (str "10." prefix-number)
-          url (str
-               (conf/get-param [:upstream :prefix-info-url])
-               prefix)
-          root (try
-                 (-> url io/reader xml/parse zip/xml-zip)
-                 (catch Exception e nil))]
-      (when root
-        (when-let [id (zx/xml1-> root :publisher :member_id)]
-          (insert-publisher!
-           collection
-           (-> id zx/text (Integer/parseInt))
-           prefix
-           (zx/text (zx/xml1-> root :publisher :publisher_name))
-           (zx/text (zx/xml1-> root :publisher :publisher_location))))))))
+  (m/with-mongo (conf/get-service :mongo)
+    (ensure-publisher-indexes! collection)
+    (doseq [prefix-number (range 1000 100000)]
+      (let [prefix (str "10." prefix-number)
+            url (str
+                 (conf/get-param [:upstream :prefix-info-url])
+                 prefix)
+            root (try
+                   (-> url io/reader xml/parse zip/xml-zip)
+                   (catch Exception e nil))]
+        (when root
+          (when-let [id (zx/xml1-> root :publisher :member_id)]
+            (insert-publisher!
+             collection
+             (-> id zx/text (Integer/parseInt))
+             prefix
+             (zx/text (zx/xml1-> root :publisher :publisher_name))
+             (zx/text (zx/xml1-> root :publisher :publisher_location)))))))))
 
 (def date-format (df/formatter "yyyy-MM-dd"))
 
