e8dfcba31eff7c33e12e53642e7abd738159e69c
Karl Jonathan Ward <karl.j.ward@gmail.com>
2014-03-04 10:32:53 +0000

    Bump ring-logstash version

--- project.clj
+++ project.clj
@@ -51,7 +51,7 @@
                  [incanter "1.5.4"]
                  [compliment "0.0.3"]
                  [instaparse "1.2.14"]
-                 [kjw/ring-logstash "0.1.2"]
+                 [kjw/ring-logstash "0.1.3"]
                  [crossref/heartbeat "0.1.1"]])
 
 
