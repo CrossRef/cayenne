1516f5c4f861f6c3c2437b40707aeb673d88132d
Karl Jonathan Ward <karl.j.ward@gmail.com>
2014-03-26 12:44:37 +0000

    Deposit xml batch id alteration and doi extraction

--- src/cayenne/api/deposit.clj
+++ src/cayenne/api/deposit.clj
@@ -1,5 +1,9 @@
 (ns cayenne.api.deposit
-  (:require [clojure.xml :refer xml])
+  (:require [clojure.data.xml :as xml]
+            [clojure.zip :as zip]
+            [clojure.data.zip :as dzip]
+            [clojure.data.zip.xml :as zx]
+            [clojure.string :as string])
   (:import [java.util UUID]))
 
 ;; handle deposit dispatch based on deposited content type
@@ -10,22 +14,38 @@
   (DepositContext.
    content-type
    object
-   (UUID.)))
+   (.toString (UUID/randomUUID))))
 
 (defmulti deposit! :content-type)
 
+(defn parse-xml [context]
+  (assoc context :xml (-> context :object xml/parse zip/xml-zip)))
+
+(defn parse-deposit-dois [context]
+  (assoc context :dois
+         (zx/xml-> (:xml context) dzip/descendants :doi_data :doi zx/text)))
+
+(defn parse-partial-deposit-dois [context]
+  (assoc context :dois
+         (zx/xml-> (:xml context) :body dzip/children-auto :doi zx/text)))
+
+(defn alter-xml-batch-id [context]
+  (let [batch-doi-loc (zx/xml1-> (:xml context) :head :doi_batch_id)]
+    (assoc context :xml
+           (zip/xml-zip
+            (zip/root
+             (zip/edit batch-doi-loc
+                       #(assoc % :content [(:batch-id context)])))))))
+
 (defmethod deposit! "application/vnd.crossref.deposit+xml" [context]
-  (let [xml-object (-> context :object xml/parse)]
-    
-    
-    
-  ; parse xml
-  ; replace batch-id
-  ; pull out DOIs
-  ; record in mongo
-  ; deposit
-  ())
+  (-> context
+      parse-xml
+      parse-deposit-dois
+      alter-xml-batch-id))
 
 (defmethod deposit! "application/vnd.crossref.partial+xml" [context]
-  ())
+  (-> context
+      parse-xml
+      parse-partial-deposit-dois
+      alter-xml-batch-id))
   
