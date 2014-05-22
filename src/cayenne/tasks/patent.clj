11fd7fce1ec82bfea4801643a16408fd6813358f
Karl Jonathan Ward <karl.j.ward@gmail.com>
2014-05-22 13:12:13 +0100

    Enable test patent citation csv deposits

--- src/cayenne/tasks/patent.clj
+++ src/cayenne/tasks/patent.clj
@@ -13,18 +13,19 @@
             doi (-> citation-line second doi-id/normalize-long-doi)
             language (-> citation-line (nth 2) string/lower-case string/trim)
             patent-title (-> citation-line (nth 3) string/trim)]
-        (when-not (m/fetch-one "citations" :where {"from.id" patent-key
-                                                   "to.id" doi})
-          (m/insert! "citations"
-                     {:from {:type :patent :id patent-key :authority :cambia}
-                      :to {:type :doi :id doi :authority :crossref}
-                      :batch-id batch-id}))
-        (when-not (m/fetch-one "patents" :where {"patent_key" patent-key})
-          (m/insert! "patents"
-                     {:patent_key patent-key
-                      :pub_key patent-key
-                      :lang language
-                      :title patent-title}))))))
+        (when consume
+          (when-not (m/fetch-one "citations" :where {"from.id" patent-key
+                                                     "to.id" doi})
+            (m/insert! "citations"
+                       {:from {:type :patent :id patent-key :authority :cambia}
+                        :to {:type :doi :id doi :authority :crossref}
+                        :batch-id batch-id}))
+          (when-not (m/fetch-one "patents" :where {"patent_key" patent-key})
+            (m/insert! "patents"
+                       {:patent_key patent-key
+                        :pub_key patent-key
+                        :lang language
+                        :title patent-title})))))))
                         
 
 
