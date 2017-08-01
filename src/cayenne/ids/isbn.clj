5d32df4d5878031a701404a80d9929b959a95fe2
Karl Jonathan Ward <karl.j.ward@gmail.com>
2017-08-01 12:33:25 +0100

    Normalize indexed and query ISBNs to dashless
    Fixes CrossRef/rest-api-doc#250 after re-index

--- src/cayenne/ids/isbn.clj
+++ src/cayenne/ids/isbn.clj
@@ -1,5 +1,6 @@
 (ns cayenne.ids.isbn
-  (:require [cayenne.ids :as ids]))
+  (:require [cayenne.ids :as ids]
+            [clojure.string :as string]))
 
 (defn is-isbn?
   [s]
@@ -11,8 +12,12 @@
      (nth (re-find #"^(http\:\/\/id\.crossref\.org\/isbn\/)(.*)" s) 2)
      s)))
 
-(defn normalize-isbn [s] 
-  (extract-isbn s))
+(defn normalize-isbn [s]
+  (when s
+    (-> s
+        extract-isbn
+        (string/replace #"[^0-9xX]" "")
+        (string/replace #"x" "X"))))
 
 (defn to-isbn-uri
   "Find anything in s that looks like it may be an ISBN and return it
