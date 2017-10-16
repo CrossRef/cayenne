96bfe2392a291a7329ad8ad3ad4217bdadee5aae
Karl Jonathan Ward <karl.j.ward@gmail.com>
2017-10-16 13:22:27 +0100

    Switch to one type per index, for ES 6+ compatibility

--- src/cayenne/elastic/mappings.clj
+++ src/cayenne/elastic/mappings.clj
@@ -20,7 +20,10 @@
   {:value {:type "string"}
    :kind  {:type "string"}})
 
-(def funder-properties)
+(def funder-properties
+  {:funder-name {:type "string"}
+   :funder-doi  {:type "string"}
+   :awards      {:type "string"}})
 
 (def update-properties)
 
@@ -30,7 +33,15 @@
 
 (def license-properties)
 
-(def assertion-properties)
+(def assertion-properties
+  {:name            {:type "string"}
+   :label           {:type "string"}
+   :group-name      {:type "string"}
+   :group-label     {:type "string"}
+   :url             {:type "string"}
+   :value           {:type "string"}
+   :order           {:type "integer"}
+   :explanation-url {:type "string"}})
 
 (def relation-properties)
 
@@ -46,20 +57,20 @@
    :language-title  {:type "string"}
    :original-title  {:type "string"}
    :container-title {:type "string"}
-   :issns           {:type "object" :properties issn-properties}
-   :isbns           {:type "object" :properties isbn-properties}
-   :contribtuors    {:type "object" :properties contributor-properties}
-   :funders         {:type "object" :properties funder-properties}
-   :awards          {:type "object" :properties award-properties}
-   :updated-by      {:type "object" :properties update-properties}
-   :updates         {:type "object" :properties update-properties}
-   :clinical-trials {:type "object" :properties clinical-trial-properties}
-   :events          {:type "object" :properties event-properties}
-   :links           {:type "object" :properties link-properties}
-   :licenses        {:type "object" :properties license-properties}
-   :assertions      {:type "object" :properties assertion-properties}
-   :relations       {:type "object" :properties relation-properties}
-   :references      {:type "object" :properties reference-properties}})
+   :issns           {:type "nested" :properties issn-properties}
+   :isbns           {:type "nested" :properties isbn-properties}
+   :contribtuors    {:type "nested" :properties contributor-properties}
+   :funders         {:type "nested" :properties funder-properties}
+   :awards          {:type "nested" :properties award-properties}
+   :updated-by      {:type "nested" :properties update-properties}
+   :updates         {:type "nested" :properties update-properties}
+   :clinical-trials {:type "nested" :properties clinical-trial-properties}
+   :events          {:type "nested" :properties event-properties}
+   :links           {:type "nested" :properties link-properties}
+   :licenses        {:type "nested" :properties license-properties}
+   :assertions      {:type "nested" :properties assertion-properties}
+   :relations       {:type "nested" :properties relation-properties}
+   :references      {:type "nested" :properties reference-properties}})
 
 (def member-properties
   {})
@@ -84,8 +95,13 @@
    "subject" {:properties subject-properties}
    "journal" {:properties journal-properties}})
   
-(defn create-mappings [conn]
-  (elastic/request conn
-                   :url "cayenne"
-                   :method :put
-                   :body {:mappings mapping-types}))
+(defn create-indexes
+  "Creates an index per top-level document type - in preparation for ES 6+
+   compatibility (which will remove multiple types per field name, making
+   multiple types per index unworkable."
+  [conn]
+  (doseq [[index-name index-data] mapping-types]
+    (elastic/request conn
+                     :url index-name
+                     :method :put
+                     :body {:mappings {"doc" index-data}})))
