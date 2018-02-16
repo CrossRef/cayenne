d21d3fd2291417fd8cf5ebfe0b8db48ddde75cb2
Mark Woodhall <mark.woodhall@gmail.com>
2018-02-16 10:44:02 +0000

    Expand on journals tests and prepare for works feed loading

--- test/cayenne/journals_test.clj
+++ test/cayenne/journals_test.clj
@@ -1,6 +1,8 @@
 (ns cayenne.journals-test
-  (:require [cayenne.conf :refer [set-param!]]
+  (:require [cayenne.api.v1.feed :refer [start-feed-processing]]
+            [cayenne.conf :refer [set-param! with-core]]
             [cayenne.tasks :refer [load-journals load-last-day-works]]
+            [cayenne.tasks.solr :refer [start-insert-list-processing]]
             [cayenne.api-fixture :refer [api-root api-with]]
             [clojure.test :refer [use-fixtures deftest testing is]]
             [clj-http.client :as http]))
@@ -13,7 +15,16 @@
                        (update-in [:items] (partial map #(dissoc % :last-status-check-time))))
           expected-response (read-string (slurp "test/resources/titles.edn"))]
       (is (= expected-response response))))
-  
+
+  (testing "journals endpoint returns expected result for offset"
+    (doseq [offset [20 40]]
+      (let [response (-> (http/get (str api-root "/v1/journals?offset=" offset) {:as :json})
+                         :body
+                         :message
+                         (update-in [:items] (partial map #(dissoc % :last-status-check-time))))
+            expected-response (read-string (slurp (str "test/resources/titles-offset-" offset ".edn")))]
+        (is (= expected-response response)))))
+
   (testing "journals endpoint returns expected result for ISSN"
     (doseq [issn ["2542-1298" "2303-5595"]]
       (let [response (-> (http/get (str api-root "/v1/journals/" issn) {:as :json})
@@ -21,11 +32,23 @@
                          :message
                          (dissoc :last-status-check-time))
             expected-response (read-string (slurp (str "test/resources/titles/" issn ".edn")))]
-        (is (= expected-response response))))))
+        (is (= expected-response response)))))
+  
+  (comment (testing "journals endpoint returns expected result for ISSN works"
+    (doseq [issn ["2542-1298" "2303-5595"]]
+      (let [response (-> (http/get (str api-root "/v1/journals/" issn "/works") {:as :json})
+                         :body
+                         :message
+                         (dissoc :last-status-check-time))
+            expected-response (read-string (slurp (str "test/resources/titles/" issn "-works.edn")))]
+        (is (= expected-response response)))))))
 
 (use-fixtures 
   :once 
-  (api-with (fn []
-              (comment (load-last-day-works))
-              (set-param! [:location :cr-titles-csv] "test/resources/titles.csv") 
-              (load-journals))))
+  (api-with 
+    (fn []
+      (with-core :default 
+        (comment (set-param! [:dir :data] "test/resources/works/"))
+        (set-param! [:location :cr-titles-csv] "test/resources/titles.csv")) 
+      (load-journals)
+      (comment (start-feed-processing)))))
