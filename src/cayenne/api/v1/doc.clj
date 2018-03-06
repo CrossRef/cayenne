6943b2bb4b37af6c029f4dde562126730da422e7
Mark Woodhall <mark.woodhall@gmail.com>
2018-03-06 17:13:06 +0000

    Start adding journal endpoints to swagger docs
    Minor changes to funders schema

--- src/cayenne/api/v1/doc.clj
+++ src/cayenne/api/v1/doc.clj
@@ -22,15 +22,28 @@
                                :email "support@crossref.org"
                                :url "https://crossref.org"}}
               :tags [{:name "funder"
-                      :description "Endpoints that expose funder related data"}]
+                      :description "Endpoints that expose funder related data"}
+                     {:name "journal"
+                      :description "Endpoints that expose journal related data"}]
               :paths {"/funders" {:get {:description "Gets a collection of funders"
                                         :parameters (merge-with merge sc/FundersFilter sc/QueryParams)
-                                        :responses {200 {:schema sc/FundersMessage
+                                        :responses {200 {:schema sc/Funders
                                                          :description "A list of funders."}}
                                         :tags ["funder"]}}
                       "/funders/:id" {:get {:description "Gets a specific funder by it's id, as an example use id 501100006004"
                                             :parameters {:path {:id sc/FunderId}}
-                                            :responses {200 {:schema sc/Funders
+                                            :responses {200 {:schema sc/FunderMessage
                                                              :description "The funder identified by {id}."}
                                                         404 {:description "The funder identified by {id} does not exist."}}
-                                            :tags ["funder"]}}}})))))
+                                            :tags ["funder"]}}
+                      "/journals" {:get {:description "Gets a collection of journals"
+                                         :parameters sc/QueryParams
+                                         :responses {200 {:schema sc/Journals
+                                                          :description "A list of journals"}}
+                                         :tags ["journal"]}}
+                      "/journals/:issn" {:get {:description "Gets a specific journal by it's issn, as an example use id 03064530"
+                                               :parameters {:path {:id sc/JournalIssn}}
+                                               :responses {200 {:schema sc/JournalMessage
+                                                                :description "The journal identified by {issn}."}
+                                                           404 {:description "The journal identified by {issn} does not exist."}}
+                                               :tags ["journal"]}}}})))))
--- src/cayenne/api/v1/schema.clj
+++ src/cayenne/api/v1/schema.clj
@@ -4,8 +4,20 @@
             [ring.swagger.swagger2 :as rs]
             [schema.core :as s]))
 
-(s/defschema Query {:start-index s/Int :search-terms s/Str})
+;; Generic
+(s/defschema Query 
+  {:start-index s/Int :search-terms s/Str})
 
+(s/defschema Message 
+  {:status s/Str
+   :message-type s/Str
+   :message-version s/Str})
+
+(s/defschema QueryParams 
+  {:query {:rows (field s/Int {:description "The number of rows to return" :required false})
+           :offset (field s/Int {:description "The number of rows to skip before returning" :required false})}})
+
+;; Funders
 (s/defschema FunderId (field s/Str {:description "The id of the funder"}))
 (s/defschema Funder {:id FunderId,
                      :location (field s/Str {:description "The geographic location of the funder"})
@@ -16,16 +28,11 @@
                      :replaced-by [s/Str]
                      :tokens [s/Str]})
 
-(s/defschema Message {:status s/Str
-                      :message-type s/Str
-                      :message-version s/Str})
-
-(s/defschema Funders {:items-per-page s/Int
-                      :query Query
-                      :total-results s/Int
-                      :items [Funder]})
-
-(s/defschema FundersMessage (merge Message {:message Funders}))
+(s/defschema FunderMessage (merge Message {:message Funder}))
+(s/defschema Funders (merge Message {:message {:items-per-page s/Int
+                                               :query Query
+                                               :total-results s/Int
+                                               :items [Funder]}}))
 
 (s/defschema 
   FundersFilter 
@@ -34,6 +41,52 @@
                           :required false
                           :pattern "location:.*"})}})
 
-(s/defschema QueryParams 
-  {:query {:rows (field s/Int {:description "The number of rows to return" :required false})
-           :offset (field s/Int {:description "The number of rows to skip before returning" :required false})}})
+;; Journals
+(s/defschema JournalIssn (field [s/Str] {:description "The ISSN identifiers associated with the journal"}))
+(s/defschema JournalCoverage 
+  {:affiliations-current s/Int
+   :funders-backfile s/Int
+   :licenses-backfile s/Int
+   :funders-current s/Int
+   :affiliations-backfile s/Int
+   :resource-links-backfile s/Int
+   :orcids-backfile s/Int
+   :update-policies-current s/Int
+   :orcids-current s/Int
+   :references-backfie s/Int
+   :award-numbers-backfile s/Int
+   :update-policies-backfile s/Int
+   :licenses-current s/Int
+   :award-numbers-current s/Int
+   :abstracts-backfile s/Int
+   :resource-links-current s/Int
+   :abstracts-current s/Int
+   :references-current s/Int})
+
+(s/defschema JournalCounts
+  {:total-dois s/Int
+   :current-dois s/Int
+   :backfile-dois s/Int})
+
+(s/defschema JournalIssnType 
+  {:value s/Str
+   :type s/Str})
+
+(s/defschema Journal 
+  {:title (field s/Str {:description "The title of the journal"}) ,
+   :publisher (field s/Str {:description "The publisher of the journal"})
+   :last-status-check-time s/Int
+   :counts JournalCounts
+   :dois-by-issued-year [[s/Int s/Int]]
+   :coverage JournalCoverage
+   :subjects [s/Str]
+   :issn-type JournalIssnType
+   :ISSN JournalIssn})
+
+(s/defschema JournalMessage
+  (merge Message {:message Journal}))
+
+(s/defschema Journals (merge Message {:message {:items-per-page s/Int
+                                                :query Query
+                                                :total-results s/Int
+                                                :items [Journal]}}))
