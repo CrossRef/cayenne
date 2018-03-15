b38a55e6e97b8946aefd05a30cba22aab5c5d60d
Mark Woodhall <mark.woodhall@gmail.com>
2018-03-15 14:13:07 +0000

    Add a test for transforming a doi to rdf+xml

--- test/cayenne/works_test.clj
+++ test/cayenne/works_test.clj
@@ -1,5 +1,6 @@
 (ns cayenne.works-test
   (:require [cayenne.api-fixture :refer [api-root api-get api-with-works]]
+            [clj-http.client :as http]
             [clojure.data.json :refer [write-str]]
             [clojure.test :refer [use-fixtures deftest testing is]]
             [clojure.java.io :refer [resource]]))
@@ -28,6 +29,14 @@
       (let [response (api-get (str "/v1/works?filter=" q-filter))
             expected-response (read-string (slurp (resource (str "works/?filter=" q-filter ".edn"))))]
         (is (= expected-response response) (str "unexpected result for filter " q-filter)))))
+
+  (testing "works endpoint returns expected result for transform"
+    (doseq [doi ["10.1016/j.psyneuen.2016.10.018" "10.7287/peerj.2196v0.1/reviews/2"]]
+      (let [response (->> {:accept "application/rdf+xml"} 
+                          (http/get (str api-root "/v1/works/" doi "/transform") )
+                          :body)
+            expected-response (slurp (resource (str "works/" doi "-transform.rdf")))]
+        (is (= expected-response response) (str "unexpected result for transform of " doi)))))
 
   (testing "works endpoint returns expected result for DOI agency"
     (with-redefs 
