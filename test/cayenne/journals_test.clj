45eeb600f5f17bdb6c9bc4a7f13b65e61698798a
Joe Wass <jwass@crossref.org>
2019-03-14 16:44:09 +0000

    Fix journals test #76  - These had the same name, and so were aliased, and didn't run.

--- test/cayenne/journals_test.clj
+++ test/cayenne/journals_test.clj
@@ -3,33 +3,35 @@
             [clojure.java.io :refer [resource]]
             [clojure.test :refer [use-fixtures deftest testing is]]))
 
-(deftest ^:integration querying-journals
-  (testing "journals endpoint returns expected result"
-    (let [response (api-get "/v1/journals")
-          expected-response (read-string (slurp (resource "titles.edn")))]
-      (is (= expected-response response)))))
+(def present-issns
+  "These are known to be present in the test fixture titles.csv file"
+  #{"2542-1298" "2303-5595"})
 
-(deftest ^:integration querying-journals
-  (testing "journals endpoint returns expected result for offset"
-    (doseq [offset [20 40]]
-      (let [response (api-get (str "/v1/journals?offset=" offset))
-            expected-response (read-string (slurp (resource (str "titles-offset-" offset ".edn"))))]
-        (is (= expected-response response))))))
+(deftest ^:integration all-journals
+  (testing "Journals endpoint includes all known journals."
+    (let [response (api-get "/v1/journals")]
+      (is (every? (->> response :items (mapcat :ISSN) set) present-issns)
+          "Every known ISSN should be present in the response."))))
 
-(deftest ^:integration querying-journals
-  (testing "journals endpoint returns expected result for ISSN"
-    (doseq [issn ["0306-4530" "2542-1298" "2303-5595"]]
-      (let [response (api-get (str "/v1/journals/" issn))
-            expected-response (read-string (slurp (resource (str "titles/" issn ".edn"))))]
-        (is (= expected-response response))))))
+(deftest ^:integration journal-by-issn
+  (testing "Journal endpoint returns journals with the given ISSN."
+    (doseq [issn present-issns]
+      (let [response (api-get (str "/v1/journals/" issn))]
 
-(deftest ^:integration querying-journals
-  (testing "journals endpoint returns expected result for ISSN works"
-    (doseq [issn ["0306-4530"]]
-      (let [response (-> (api-get (str "/v1/journals/" issn "/works?rows=76"))
-                         no-scores)
-            expected-response (read-string (slurp (resource (str "titles/" issn "-works.edn"))))]
-        (is (= expected-response response))))))
+        (is (-> response :ISSN set (contains? issn))
+            "ISSN should be returned")
+        (is (contains? (->> response :issn-type (map :value) set)
+                       issn)
+            "ISSN should be present in one of the issn types")))))
+
+(deftest ^:integration works-by-journal
+  (testing "All works retrieved by ISSN have the correct ISSN."
+           ; NB Weirdly, this one isn't present in the title file.
+           (let [issn "0306-4530"
+                 response (api-get (str "/v1/journals/" issn "/works"))]
+        (is (-> response :items not-empty) "Some results returned.")
+        (is (->> response :items (mapcat :ISSN) set (every? #{issn}))
+            "Every item should have the given ISSN"))))
 
 (use-fixtures
   :once
